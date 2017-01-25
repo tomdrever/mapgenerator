bcb26a9e370bdf70a2cbaa8ad76ffa5f3fd5238b
charlesstewart <tomdr1001@outlook.com>
2017-01-25 12:17:31 +0000

    Add and implement settings, tidy up algorithm code

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var canvasStyle = window.getComputedStyle(canvas, null)
 var canvasWidth = parseInt(canvasStyle.width.replace("px", ""))
 var canvasHeight = parseInt(canvasStyle.height.replace("px", ""))
 
+var processing = document.getElementById("processing_wrapper")
+
+var gridSizeInput = document.getElementById("grid-size-input")
+var gridSmoothnessInput = document.getElementById("grid-smoothness-input")
+
 newMap()
 
 function onDownloadClicked(link) {
@@ -22,18 +27,25 @@ function onCanvasClicked() {
 }
 
 function newMap() {
+  processing.style.display = "flex"
+
   context.clearRect(0, 0, canvasWidth, canvasHeight)
 
-  // TODO - get these values in settings?
-  var gridSize = 513
+  var gridSize = Math.pow(2, gridSizeInput.value) + 1
+  var smoothness = gridSmoothnessInput.value;
+
   var cellSize = canvasWidth / gridSize
 
-  var grid = getGrid(gridSize)
+  var grid = getGrid(gridSize, smoothness)
 
-  for (var x = 0; x < gridSize; x++) {
-    for (var y = 0; y < gridSize; y++) {
-      context.fillStyle = "rgb({0}, {1}, {2})".format(grid[x][y].r, grid[x][y].g, grid[x][y].b)
-      context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize)
+  setTimeout(function() {
+    for (var x = 0; x < gridSize; x++) {
+      for (var y = 0; y < gridSize; y++) {
+        context.fillStyle = "rgb({0}, {1}, {2})".format(grid[x][y].r, grid[x][y].g, grid[x][y].b)
+        context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize)
+      }
     }
-  }
+
+    processing.style.display = "none"
+  }, 50)
 }
--- mapgen.js
+++ mapgen.js
@@ -6,12 +6,9 @@ class Cell {
   }
 }
 
-function diamondSquare(DATA_SIZE) {
-	//DATA_SIZE is the size of grid to generate, note this must be a
-	//value 2^n+1
+function diamondSquare(DATA_SIZE, smoothness) {
 
-	//an initial seed value for the corners of the data
-	var SEED = 128.0;
+  // Create an empty grid
 	var data = new Array(DATA_SIZE);
 	for (var i = 0; i < DATA_SIZE; i++) {
 		data[i] = new Array(DATA_SIZE);
@@ -20,92 +17,59 @@ function diamondSquare(DATA_SIZE) {
       data[i][j] = 0;
     }
 	}
-	//seed the data
+
+	// Seed the data
+  var SEED = 128.0;
 	data[0][0] = data[0][DATA_SIZE - 1] = data[DATA_SIZE - 1][0] = data[DATA_SIZE -1][DATA_SIZE - 1] = SEED;
 
-	var h = 128.0;//the range (-h -> +h) for the average offset
-
-	//side length is distance of a single square side
-	//or distance of diagonal in diamond
-	for (var sideLength = DATA_SIZE - 1;
-		//side length must be >= 2 so we always have
-		//a new value (if its 1 we overwrite existing values
-		//on the last iteration)
-        sideLength >= 2;
-		//each iteration we are looking at smaller squares
-		//diamonds, and we decrease the variation of the offset
-        sideLength /= 2, h /= 2.0) {
-		//half the length of the side of a square
-		//or distance from diamond center to one corner
-		//(just to make calcs below a little clearer)
+  // This iterates the algorithm (based on smoothness), increasing the "depth"
+	for (var sideLength = DATA_SIZE - 1; sideLength >= 2; sideLength /= 2, smoothness /= 2.0) {
 		var halfSide = sideLength / 2;
 
-		//generate the new square values
+    // SQUARE
 		for (var x = 0; x < DATA_SIZE - 1; x += sideLength) {
 		    for (var y = 0; y < DATA_SIZE - 1; y += sideLength) {
-		        //x, y is upper left corner of square
-		        //calculate average of existing corners
-		        var avg = data[x][y] + //top left
-                data[x + sideLength][y] +//top right
-                data[x][y + sideLength] + //lower left
-                data[x + sideLength][y + sideLength];//lower right
+
+            // Get the average of the four surrounding corners
+		        var avg = data[x][y] +
+                data[x + sideLength][y] +
+                data[x][y + sideLength] +
+                data[x + sideLength][y + sideLength];
 		        avg /= 4.0;
 
-		        //center is average plus random offset
-		        data[x + halfSide][y + halfSide] = avg + (Math.random() * 2 * h) - h;
-                //We calculate random value in range of 2h
-                //and then subtract h so the end value is
-                //in the range (-h, +h)
-
+            // Set the square value to the average + a random number affected by the smoothness
+		        data[x + halfSide][y + halfSide] = avg + (Math.random() * 2 * smoothness) - smoothness;
 		    }
 		}
 
-		//generate the diamond values
-		//since the diamonds are staggered we only move x
-		//by half side
-		//NOTE: if the data shouldn't wrap then x < DATA_SIZE
-		//to generate the far edge values
+    // DIAMOND
 		for (var x = 0; x < DATA_SIZE - 1; x += halfSide) {
-		    //and y is x offset by half a side, but moved by
-		    //the full side length
-		    //NOTE: if the data shouldn't wrap then y < DATA_SIZE
-		    //to generate the far edge values
 		    for (var y = (x + halfSide) % sideLength; y < DATA_SIZE - 1; y += sideLength) {
-		        //x, y is center of diamond
-		        //note we must use mod  and add DATA_SIZE for subtraction
-		        //so that we can wrap around the array to find the corners
-		        var avg =
-                    data[(x - halfSide + DATA_SIZE - 1) % (DATA_SIZE - 1)][y] + //left of center
-                    data[(x + halfSide) % (DATA_SIZE - 1)][y] + //right of center
-                    data[x][(y + halfSide) % (DATA_SIZE - 1)] + //below center
-                    data[x][(y - halfSide + DATA_SIZE - 1) % (DATA_SIZE - 1)]; //above center
 
+            // Get the average of the four points on the diamond
+		        var avg =
+                    data[(x - halfSide + DATA_SIZE - 1) % (DATA_SIZE - 1)][y] +
+                    data[(x + halfSide) % (DATA_SIZE - 1)][y] +
+                    data[x][(y + halfSide) % (DATA_SIZE - 1)] +
+                    data[x][(y - halfSide + DATA_SIZE - 1) % (DATA_SIZE - 1)];
 		        avg /= 4.0;
 
-		        //new value = average plus random offset
-		        //We calculate random value in range of 2h
-		        //and then subtract h so the end value is
-		        //in the range (-h, +h)
-		        avg = avg + (Math.random() * 2 * h) - h;
-		        //update value for center of diamond
+		        avg = avg + (Math.random() * 2 * smoothness) - smoothness;
 		        data[x][y] = avg;
 
-		        //wrap values on the edges, remove
-		        //this and adjust loop condition above
-		        //for non-wrapping values.
+            // Wrap the values
 		        if (x == 0) data[DATA_SIZE - 1][y] = avg;
 		        if (y == 0) data[x][DATA_SIZE - 1] = avg;
 		    }
 		}
 	}
 
-	//return the data
 	return data;
 }
 
-function getGrid(size) {
+function getGrid(size, smoothness) {
   var grid = new Array(size)
-  var data = diamondSquare(size)
+  var data = diamondSquare(size, smoothness)
 
   for (var x = 0; x < size; x++) {
     grid[x] = new Array(size)
